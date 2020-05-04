1316882eb88512957bf24351d796412e04a53f8b
Peter Bellström <peter.bellstrom@kau.se>
2020-05-04 16:58:36 +0200

    Uppdaterat F3 igen..

--- F3/slutkod/kodex_2/app.js
+++ F3/slutkod/kodex_2/app.js
@@ -2,3 +2,34 @@
 
 //npm init
 //npm install express
+
+//npm install
+
+const express = require('express');
+
+//Ny express-applikation
+let app = express();
+
+app.listen(81, function() {
+    console.log('Server is running on port 81!');
+});
+
+app.get('/', function(request, response) {
+    //GET på /!
+    console.log('GET på url: / request.query', request.query);
+    //send(), sendFile(), end()
+    response.send('GET: ' + JSON.stringify(request.query));
+});
+
+app.post('/', function(request, response) {
+    //POST på /!
+    console.log('POST på url: / request.query', request.query);
+    //send(), sendFile(), end()
+    response.send('POST: ' + JSON.stringify(request.query));
+});
+
+/*
+app.put();
+app.patch();
+app.delete();
+*/
--- F3/slutkod/kodex_3/app.js
+++ F3/slutkod/kodex_3/app.js
@@ -2,3 +2,26 @@
 
 //npm init
 //npm install express
+
+const express = require( 'express' );
+
+let app = express();
+
+app.listen( 81, function () {
+    console.log( 'Server is running on port 81!' );
+});
+
+app.get('/', function( request, response) {
+    response.sendFile( __dirname + '/index.html' );
+});
+
+app.get('/merHamsterCom.html', function( request, response) {
+    response.sendFile( __dirname + '/merHamsterCom.html');
+});
+
+//Middleware -> Placeras sist efter alla route metoder.
+app.use( function( request, response  ){
+    response.status(404);
+    response.send('404 på given url');
+});
+
